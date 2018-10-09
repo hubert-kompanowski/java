e5bc70e395d1c08c5a3c619d5d9d5ff81d35962f
Hubert Kompanowski <hubert.kompanowski9@gmail.com>
2018-10-09 10:56:44 +0200

    1.2

---
+++ lab1/1.2/src/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * Klasa wywietlajaca powitaniena konsoli.
+ * @author Szymon
+ */
+public class HelloWorld {
+    /**
+     *
+     * @param argv lista argumentów przekazywana do programu
+     */
+    public static void main(String [] argv){
+        System.out.println("Hello World!");
+    }
+}
+
+
+/*
+    nie dało się skompilować ponieważ nazwa pliku - witajswiecie
+    jest różna od nazwy klasy HelloWorld
+ */
