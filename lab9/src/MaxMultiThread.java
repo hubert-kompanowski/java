781799e1240e74906967d5325b440f42ff8c66fe
Hubert Kompanowski <hubert.kompanowski9@gmail.com>
2018-12-28 13:27:11 +0100

    Wpółbieżne

--- lab9/src/MaxMultiThread.java
+++ lab9/src/MaxMultiThread.java
@@ -1,7 +1,18 @@
 public class MaxMultiThread {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
+        Thread t = new Thread(new Watek());
+        t.run();
         GenerateRandoms a = new GenerateRandoms();
         a.fill();
     }
+}
+
+class Watek implements Runnable {
+    @Override
+    public void run() {
+        for( int i=0; i<10; i++ )
+            System.out.print("Watek ");
+    }
+
 }
